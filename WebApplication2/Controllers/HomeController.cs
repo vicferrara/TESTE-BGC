c29fe4d631a2a45ea9e6f02f6b6a0fae5dda3e41
Vicente <vicferrara>
2021-08-02 12:59:02 -0300

    Adicionar arquivos de projeto.

---
+++ WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private static List<Cliente> clientes;
+
+        public ActionResult Index()
+        {
+            if (clientes == null)
+                clientes = new Cliente().GetAll();
+
+            return View(clientes);
+        }
+
+        public ActionResult NovoCliente()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult InserirCliente(Cliente cliente)
+        {
+            if (String.IsNullOrEmpty(cliente.nome))
+                return View("NovoCliente", cliente);
+
+            if (String.IsNullOrEmpty(cliente.cep))
+                return View();
+
+            if (ModelState.IsValid)
+            {
+                cliente.Inserir(cliente);
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("NovoCliente", cliente);
+            }
+
+        }
+        
+    }
+}
---
+++ WebApplication2/Models/Cliente.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Cliente
+    {
+
+        private string StringConnection = ConfigurationManager.ConnectionStrings["conexao"].ConnectionString;
+
+        SqlConnection sql = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+
+        public int idCliente { get; set; }
+        public string nome { get; set; }
+        public DateTime dataNascimento { get; set; }
+        public string cep { get; set; }
+
+        public List<Cliente> GetAll()
+        {
+
+            List<Cliente> clientes = new List<Cliente>();
+
+            clientes.Add(new Cliente()
+            {
+                nome = "aaa",
+                dataNascimento = DateTime.Now,
+                cep = "123456789"
+            });
+
+            clientes.Add(new Cliente()
+            {
+                nome = "aaa",
+                dataNascimento = DateTime.Now,
+                cep = "123456789"
+            });
+
+            clientes.Add(new Cliente()
+            {
+                nome = "aaa",
+                dataNascimento = DateTime.Now,
+                cep = "123456789"
+            });
+
+            return clientes;
+        }
+
+        public void Inserir (Cliente cliente)
+        {
+            AbrirConexao();
+
+            try
+            {
+                cmd.Connection = sql;
+
+                cmd.CommandText = "INSERT INTO dbo.Cliente(nome, dataNascimento, cep) VALUES(@nome, @dataNascimento, @cep)";
+
+                cmd.Parameters.AddWithValue("nome", cliente.nome);
+                cmd.Parameters.AddWithValue("dataNascimento", cliente.dataNascimento);
+                cmd.Parameters.AddWithValue("cep", cliente.cep);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch(Exception ex)
+            {
+                FecharConexao();
+            }
+            finally
+            {
+                FecharConexao();
+            }
+
+        }
+
+        private void AbrirConexao()
+        {
+            sql.ConnectionString = StringConnection;
+
+            sql.Open();
+
+            cmd.CommandType = System.Data.CommandType.Text;
+
+        }
+
+        private void FecharConexao()
+        {
+            sql.Close();
+        }
+
+    }
+}
